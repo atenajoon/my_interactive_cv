aeb38460826f8ab14ed13a14eead978fda743b82
Atena Razm <atenarazm@gmail.com>
2021-05-17 15:36:34 -0700

    js to update navbar on scroll and on click is done in nav.js

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,3 +1,54 @@
+
+window.onscroll = function() {navOnScroll();};
+const navbar = document.getElementById("navbar");
+
+function navOnScroll() {
+
+    let offsetMob = window.pageYOffset;
+    let offset = window.pageYOffset + 80;
+    let innerH = window.innerHeight;
+
+
+    //  making the navbar sticky on scroll
+    if (offsetMob >= innerH) {
+        navbar.classList.remove("navbar2");
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+        navbar.classList.add("navbar2");
+    }
+
+    // updating the nav activation on scroll
+    let sec2 = document.getElementById("sec2").offsetTop - 200;
+    let sec3 = document.getElementById("sec3").offsetTop - 200;
+    let sec4 = document.getElementById("sec4").offsetTop - 450;
+    
+    let nav=[];
+    nav[1] = document.getElementById("nav-item-1");
+    nav[2] = document.getElementById("nav-item-2");
+    nav[3] = document.getElementById("nav-item-3");
+    nav[4] = document.getElementById("nav-item-4");
+    
+    if (offset <= sec2) { activate(1); }
+    else if (offset <= sec3) { activate(2); }
+    else if (offset <= sec4) { activate(3); }
+    else { activate(4); }
+    
+    
+    function activate(navItem) {
+        for(i = 1; i <= 4; i++) {
+            if(i == navItem) {
+                nav[i].classList.add("active");
+            } else {
+                nav[i].classList.remove("active");
+            }
+        }
+    }
+}
+
+
+
+// updating the nav activation on click
 let myNav  = document.querySelectorAll(".nav > li");
 
 for(let i=0; i<myNav.length; i++) {
