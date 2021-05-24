c052b8be0a094393b9a873c32ff72ee3e3a6b31c
Atena Razm <atenarazm@gmail.com>
2021-05-23 17:01:59 -0700

    change all font-sizes from px to em

--- scripts/nav.js
+++ scripts/nav.js
@@ -31,17 +31,17 @@ function navOnScroll() {
     // updating the nav activation on scroll
     const SECTION_COUNT = document.querySelectorAll("section");
 
-    sec = [];
-    for(var i = 2; i <= SECTION_COUNT.length; i++) { 
-        SECTION_LENGTHS = [ "none", 200, 200, 450 ];
-        sec[i] = document.getElementById("sec" + i).offsetTop - SECTION_LENGTHS[i-1]
-        console.log("sec" + i + ": ", sec[i]);
-    }
-
+    
     let nav=[];
     for(let i=1; i<=4; i++) { nav[i] = document.getElementById("nav-item-"+i); }
-   
+    
     if(highlightSections) {
+        sec = [];
+        SECTION_LENGTHS = [ "none", 200, 200, 450 ];
+        for(var i = 2; i <= SECTION_COUNT.length; i++) { 
+            sec[i] = document.getElementById("sec" + i).offsetTop - SECTION_LENGTHS[i-1];
+        }
+        
         if (offset <= sec[2]) { activate(1); }
         else if (offset <= sec[3]) { activate(2); }
         else if (offset <= sec[4]) { activate(3); }
